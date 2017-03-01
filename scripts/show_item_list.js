44548c2bab447f1b6f7edfb10701f7eb4bbd5625
lovelydaidai <pengpenglei@foxmail.com>
2017-03-02 00:03:33 +0800

    太累了

--- scripts/show_item_list.js
+++ scripts/show_item_list.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+  // 列表
 var show_li = document.getElementById('nav_cate_list_show').getElementsByTagName('li');
 var close_list = document.getElementsByClassName('item_list_close');
 for (i=0;i<show_li.length;i++){
@@ -18,5 +19,20 @@ for (i=0;i<show_li.length;i++){
     }
 
 }
-    
+// 轮播图
+    var nav_div = document.getElementsByClassName('slide_nav')[0];
+    var nav_ul = nav_div.getElementsByTagName('ul')[0];
+    var nav_li = nav_ul.getElementsByTagName('li');
+    var top_slide_div = document.getElementById('top_slide');
+    var top_slide_ul = top_slide_div.getElementsByTagName('ul')[0];
+    var top_slide_li = top_slide_ul.getElementsByTagName('li');
+    for (i=0;i<nav_li.length;i++){
+        nav_li[i].index = i;
+        nav_li[i].onmousemove = function () {
+            for (i=0;i<nav_li.length;i++){
+                top_slide_li[i].className='fn-hide';
+            };
+            top_slide_li[this.index].className='slide_list';
+        };
+    }
 }
